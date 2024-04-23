0cfaf73cf3d5d6479a8da724993da2ede998547c
killergamer2020 <josemartinezleon123@gmail.com>
2024-04-22 20:49:52 -0400

    añadi pagina principal

--- funciones.js
+++ funciones.js
@@ -1,15 +1,16 @@
-function validarMail(){
-    let user = document.querySelector("#mail");
-    if(mail.value.length >=6){
-        user.classList.add("correct")
-        user.classList.remove("incorrect")
-        document.querySelector("#error-mail").innerHTML =""
-    }else{
-        user.classList.add("incorrect")
-        user.classList.remove("correct")
-        document.querySelector("#error-mail").innerHTML ="El usuario debe contener minimo 6 caracteres!!!"
+function validarUser(){
+    let user = document.querySelector("#user")
+    if(user.value.length >= 6){
+        user.classList.add("correcto");
+        user.classList.remove("incorrecto");
+        document.querySelector("#error-user").innerHTML = "&nbsp;";
 
-    };
+    }else{
+        user.classList.add("incorrecto");
+        user.classList.remove("correcto");
+        document.querySelector("#error-user").innerHTML = "error, ingrese 6 caracteres minimo.";
+
+    }
+
 
 }
-

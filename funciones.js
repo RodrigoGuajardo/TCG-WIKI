02f001291ce42411b4c3030823fc7f12f69b1364
RodrigoGuajardo <ror.guajardo@duocuc.cl>
2024-05-04 15:26:53 -0400

    nuevo stile y configuracion de login ademas de cambios en las cards de las paginas

--- funciones.js
+++ funciones.js
@@ -1,62 +1,43 @@
-function validarMail(){
-    let user = document.querySelector("#user")
-    if(user.value.length >= 6){
+function validarUser() {
+    let user = document.querySelector("#user");
+    if (user.value.length >= 6) {
         user.classList.add("correct");
         user.classList.remove("incorrect");
         document.querySelector("#error-user").innerHTML = "&nbsp;";
-
-    }else{
+    } else {
         user.classList.add("incorrect");
         user.classList.remove("correct");
-        document.querySelector("#error-user").innerHTML = "error, ingrese 6 caracteres minimo.";
-
+        document.querySelector("#error-user")
+            .innerHTML = "Error, ingrese 6 caracteres mínimo.";
     }
-
-
 }
 
-function validarPass(){
+function validarPass() {
     let clave = document.querySelector("#clave");
-    if(clave.value.length > 5 && clave.value.length <= 12){
+    if (clave.value.length > 5 && clave.value.length < 13) {
         clave.classList.add("correct");
         clave.classList.remove("incorrect");
         document.querySelector("#error-clave").innerHTML = "&nbsp;";
-    }else{
+    } else {
         clave.classList.add("incorrect");
         clave.classList.remove("correct");
-        document.querySelector("#error-clave").innerHTML = "error, ingrese entre 6 y 12 caracteres.";
-
+        document.querySelector("#error-clave")
+            .innerHTML = "Error, ingrese entre 6 y 12 caracteres.";
     }
-
-}
-function validarPass(){
-    let clave = document.querySelector("#clave");
-    if(clave.value.length > 5 && clave.value.length <= 12){
-        clave.classList.add("correcto");
-        clave.classList.remove("incorrecto");
-        document.querySelector("#error-clave").innerHTML = "&nbsp;";
-    }else{
-        clave.classList.add("incorrecto");
-        clave.classList.remove("correcto");
-        document.querySelector("#error-clave").innerHTML = "error, ingrese entre 6 y 12 caracteres.";
-
-    }
-
 }
 
-function validarFormulario(){
+function validarFormulario() {
     let inputs = document.querySelectorAll("input");
     let correctos = true;
     inputs.forEach(element => {
-        if(element.classList.contains("incorrecto")){
+        if (element.classList.contains("incorrect")) {
             correctos = false
         }
-        
     });
-    if(correctos){
+    if (correctos) {
         document.querySelector("form").submit();
-    }else{
-        document.querySelector("#error-form").innerHTML = "error, revise los campos";
-
+    } else {
+        document.querySelector("#error-form")
+            .innerHTML = "Error, revise los campos.";
     }
 }
